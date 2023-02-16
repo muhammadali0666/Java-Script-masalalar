cd75f56a18b68697c57a93e54a267408a9b31a83
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-02-16 16:54:38 +0500

    init

---
+++ 12-dars/index.js
@@ -0,0 +1,18 @@
+// in group 
+
+// 1-masala 
+// const swap = (a, b)=> {
+// 	[a , b] = [b , a];
+// 	return [a, b]
+// }
+
+// console.log(swap(21,12));
+
+// 2-masala 
+
+// function balancedArray(arr){
+//   let len = arr.length
+//   let res = arr.slice(0, len / 2).reduce((first, secund) => first + secund)
+//   let res1 = arr.slice(len / 2).reduce((first, secund) => first + secund)
+// }
+// console.log(balancedArray([1,2,3,4,5,6]));
